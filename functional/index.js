595442e8ff49f55678f5d4048d20f062b7ed3d82
rare1k <hi@rare1k.dev>
2024-06-06 21:17:38 -0400

    feat: start working on selection

--- functional/index.js
+++ functional/index.js
@@ -2,6 +2,8 @@ let c = document.querySelector("canvas")
 let i = document.querySelector("textarea")
 var ctx = c.getContext("2d");
 
+let TSelection = [0,0]
+let LineNum = 0;
 function splitString(str, fontSize) {    
     let width = window.innerWidth;
     let charactersPerLine = Math.floor(width / fontSize);
@@ -11,6 +13,7 @@ function splitString(str, fontSize) {
     lines.forEach(line => {
         for (let i = 0; i < line.length; i += charactersPerLine) {
             result.push(line.slice(i, i + charactersPerLine));
+            LineNum++;
         }
     });
     return result
@@ -49,5 +52,12 @@ function inputkeydown(evt){
     console.log("CTRL+A")
     evt.preventDefault();
     i.setSelectionRange(0, 0);
+    TSelection = [0,0]
+  }
+  if (evt.key == "ArrowLeft" && TSelection[0] != 0) {
+    TSelection[0] = TSelection[0] - 1
+  }
+  if (evt.key == "ArrowRight" && TSelection[0] != Math.round(window.innerWidth/10)) {
+    TSelection[0] = TSelection[0] + 1
   }
 }
